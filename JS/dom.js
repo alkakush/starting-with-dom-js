2ce40cdcd0116793d227c7eab875fbc921c46792
Alka kushwaha <crystallife4me@gmail.com>
2021-05-11 14:51:54 +0530

    Used getElementsByTagName() method

--- JS/dom.js
+++ JS/dom.js
@@ -48,7 +48,7 @@ header.style.borderBottom = 'solid 4px #ff0';
 */
 
 // ==========  GETELEMENTSBYCLASSNAME ===========================
-
+/*
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
@@ -62,4 +62,20 @@ items[1].style.backgroundColor = 'yellow' ;
 for(var i = 0; i < items.length; i++){
     items[i].style.backgroundColor = '#f4f4f4';
 }
+*/
 
+// ======================= GETELEMENTSBYTAGNAME =======================
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold' ;
+li[1].style.backgroundColor = 'yellow' ;
+
+//== Gives error
+// items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < li.length; i++){
+    li[i].style.backgroundColor = 'darkgray';
+}
