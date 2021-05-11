7c71bd3284c17ed775a6c5d4605506c6de847d04
Alka kushwaha <crystallife4me@gmail.com>
2021-05-11 11:50:54 +0530

    Examine by index no.

--- JS/dom.js
+++ JS/dom.js
@@ -1,5 +1,5 @@
 //  EXAMINE  THE DOCUMENT OBJECT 
-
+/*
 // console.dir(document);
 console.log(document.domain);
 console.log(document.url);
@@ -8,7 +8,7 @@ console.log(document.title);
 console.log(document.doctype);
 console.log(document.head);
 console.log(document.body);
+*/
 
-
-
-
+ console.log(document.all);
+ console.log(document.all[10]);
