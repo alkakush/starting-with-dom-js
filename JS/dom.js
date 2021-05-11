89af03f9d57f4e98d47feb84e572a471ccac23d4
Alka kushwaha <crystallife4me@gmail.com>
2021-05-11 14:28:44 +0530

    Used getElementsByClassName() method and  appplied  style property.

--- JS/dom.js
+++ JS/dom.js
@@ -39,9 +39,27 @@ console.log(document.links);
 
 
 //======= apply style =====
-
+/*
 var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('main-header');
 
 headerTitle.style.borderBottom = 'solid 4px #000';
 header.style.borderBottom = 'solid 4px #ff0';
+*/
+
+// ==========  GETELEMENTSBYCLASSNAME ===========================
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold' ;
+items[1].style.backgroundColor = 'yellow' ;
+
+//== Gives error
+// items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
+
