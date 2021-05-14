d8dd383a8c2666dca842ae9762adc469f18009e0
alka kushwaha <crystallife4me@gmail.com>
2021-05-14 15:36:27 +0530

    Used form event(add and delete )  submit and input value in form , And  delete input values

---
+++ JS/main.js
@@ -0,0 +1,59 @@
+
+// =========================== Use form submit Event and item Delete event ================== //
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// == Form submit Event == //
+form.addEventListener('submit', addItem);
+
+//=== Delete event ===//
+itemList.addEventListener('click', removeItem);
+
+// Add item
+function addItem(e){
+    e.preventDefault();
+
+    console.log(1);
+
+    // get input value
+    var newItem = document.getElementById('item').value;
+
+   // Create new li element
+   var li = document.createElement('li');
+   // Add class 
+    li.className = 'list-group-item';
+    // console.log(li);
+
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+     
+    // Create Delete button element
+    var deleteBtn = document.createElement('button');
+
+    // Add class to delete button 
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Append  button to the li
+    li.appendChild(deleteBtn);
+
+    // Append li to list
+    itemList.appendChild(li);
+}
+
+// === Remove item ===//
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+     console.log(1); 
+     if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li); 
+     }  
+    }
+
+    // console.log(1);
+}
